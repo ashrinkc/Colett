0a83a7880c0baea015c8ba2fecb1dfddde1e7f76
Ashrin K.C <ashrinkc3@gmail.com>
2022-09-02 17:15:39 +0545

    added dynamic pages and responsiveness

--- src/Pages/OurHomes/OurHomeGallery.js
+++ src/Pages/OurHomes/OurHomeGallery.js
@@ -37,7 +37,7 @@ const OurHomeGallery = () => {
         <h1>{name}</h1>
         <h5>{location}</h5>
         <div className="mainGalleryh">
-          <div className="gallery">
+          <div className="galleryh">
             <div className="galleryHead">
               <div className="galleryHeadoneh">
                 <div>

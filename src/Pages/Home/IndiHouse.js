96737c768993a5436cea778f18efa1d03624bf29
Ashrin K.C <ashrinkc3@gmail.com>
2022-09-02 15:36:26 +0545

    changed navbar

--- src/Pages/Home/IndiHouse.js
+++ src/Pages/Home/IndiHouse.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const IndiHouse = (props) => {
+  const navigate = useNavigate();
   return (
     <div className="indiHouse">
       <div className="indiHouseInfo">
         <img src={props.item.img} />
         <h3>{props.item.title}</h3>
         <p>{props.item.desc}</p>
-        <button>Find Out More</button>
+        <button onClick={() => navigate("/ourHome")}>Find Out More</button>
       </div>
     </div>
   );
